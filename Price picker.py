353f79552b7d762bc546dee7f8a86a81fc19adb4
mateusluzzi <matluzzi@hotmail.com>
2021-06-26 00:26:14 -0300

    Inserindo num DF

--- Price picker.py
+++ Price picker.py
@@ -1,3 +1,23 @@
 # %%
 import os
-import pandas
+import pandas as pd
+import requests
+
+# %%
+
+API = "APUO17BD0D08LYO3"
+
+# %%
+def TIME_SERIES_INTRADAY(symbol,interval):
+    sym = "&symbol="+symbol
+    interv = "&interval="+interval
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY"+sym+interv+"&apikey="+API
+    r = requests.get(url).json()
+    del r['Meta Data']
+    r = r['Time Series (5min)']
+    data = pd.DataFrame.from_dict(r,orient='index')
+    return data
+# %%
+TIME_SERIES_INTRADAY("UBER","5min")
+
+
