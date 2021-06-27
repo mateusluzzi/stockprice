f8947da7d05d51b06f18a41ebcbf3b253a89a800
mateusluzzi <matluzzi@hotmail.com>
2021-06-27 03:19:53 -0300

    Commit de apis

--- Price picker.py
+++ Price picker.py
@@ -1,52 +1,78 @@
 # %%
-import os
 import pandas as pd
-from pandas.core.accessor import register_dataframe_accessor
 import requests
-import json
-import io
+from alpha_vantage.timeseries import TimeSeries
 # %%
 
 API_KEY = "APUO17BD0D08LYO3"
 API_URL = "Https://www.alphavantage.co/query"
 # %%
-tickers = ['UBER']
+ts = TimeSeries(key=API_KEY,output_format='pandas')
+ts.get_symbol_search('PETR')
+
+# %%
+tickers = ['ITUB','PETR4.SAO']
 # %%
 def intraday(tickers):
+    intraday = pd.DataFrame()
+    z = 0
     for s in tickers:
-        z = 0
         params = {"function": "TIME_SERIES_INTRADAY",
                 "symbol": tickers[z],
                 "interval": "15min",
+                "outputsize": "full",
                 "datatype" : "json",
                 "apikey":API_KEY}
-        response = requests.get(API_URL, params)
-        data = response.json()
-        data = pd.DataFrame.from_dict(data["Time Series (15min)"],orient='index')
-        data['ticker'] = params['symbol']
-        print(data)
+        try:
+            response = requests.get(API_URL, params)
+            data = response.json()
+            intraday2 = pd.DataFrame.from_dict(data["Time Series (15min)"],orient='index')
+            intraday2 = intraday2.iloc[0:20000,:]
+            intraday2['ticker'] = params['symbol']
+            intraday = pd.concat([intraday,intraday2])
+            print(intraday)
+        except:
+            pass
         z += 1
 # %%
 intraday(tickers[:])
-
-
-# data.columns = ['1. open','2. high']
-# data.sort_values(['1. open','2. high'], input = True)
-# data.reset_index(drop=True, inplace=True)
-# data
 # %%
-def TIME_SERIES_INTRADAY(symbol,interval):
-    for s in symbol:
-        sym = "&symbol="+s
-        interv = "&interval="+interval
-        url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY"+sym+interv+"&apikey="+API
-        r = requests.get(url).json()
-        del r['Meta Data']
-        r = r['Time Series (5min)']
-        data = pd.DataFrame.from_dict(r,orient='index')
-        print(s)
-        data['ticker'] = s
-        return data
+def daily_adjusted(tickers):
+    daily_adjusted = pd.DataFrame()
+    z = 0
+    for s in tickers:    
+        params = {"function":"TIME_SERIES_DAILY_ADJUSTED",
+                "symbol" : tickers[z],
+                "outputsize" : "compact",
+                "apikey" : API_KEY}
+
+        response = requests.get(API_URL, params)
+        data = response.json()
+        daily_adjusted2 = pd.DataFrame.from_dict(data["Time Series (Daily)"],orient='index')
+        daily_adjusted2 = daily_adjusted2.iloc[0:20000,:]
+        daily_adjusted2['ticker'] = params['symbol']
+        daily_adjusted = pd.concat([daily_adjusted,daily_adjusted2])
+        print(daily_adjusted)
+        z += 1
+
 # %%
-TIME_SERIES_INTRADAY(["JPSA."],"5min")
+daily_adjusted(tickers[:])
+# %%
+
+def overview(tickers):
+        data = {}
+        z = 0
+        for s in tickers:
+            params = {"function":"OVERVIEW",
+                    "symbol" : tickers[z],
+                    "apikey" : API_KEY}
+
+            response = requests.get(API_URL, params)
+            dict = response.json()
+            data[s] = dict
+            z += 1
+        overview = pd.DataFrame.from_dict(data,orient='index').reset_index()    
+
+# %%
+overview(tickers[:])
 
